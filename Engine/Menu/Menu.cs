1490477239e7d1f58799faa57c81d73880fb69fd
orcwarrior <orcwarrior@o2.pl>
2012-11-28 17:35:39 +0100

    t

---
+++ Engine/Menu/Menu.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Battle_Tanks.Engine
+{
+	public class Menu
+	{
+		private List <Sprite> _backgroundVisuals;
+		menuItem selectedItem;
+
+		public List<menuItems> items
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+			set
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public void addMenuItem(menuItem itm)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void selectPreviousItem()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void selectNextItem()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Render()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
